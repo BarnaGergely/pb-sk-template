e30e6a3fc93941d1adb75e23f158ba58f5839418
Gergely Barna <gergely.barna.ext@evosoft.com>
2025-09-04 14:43:07 +0200

    extend pocketbase SDK functionality eoth auth related methods

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,4 +1,33 @@
 import { PUBLIC_POCKETBASE_API_URL } from '$env/static/public';
-import PocketBase from 'pocketbase';
+import PocketBase, { type AuthRecord, type RecordAuthResponse, type RecordModel } from 'pocketbase';
 
-export const pocketBase: PocketBase = new PocketBase(PUBLIC_POCKETBASE_API_URL);
+/**
+ * Custom features for the PocketBase JavaScript SDK
+ */
+class ExtendedPocketBase extends PocketBase {
+	async login(email: string, password: string): Promise<RecordAuthResponse<RecordModel>> {
+		return this.collection('users').authWithPassword(email, password);
+	}
+
+	async register(email: string, password: string, passwordConfirm: string): Promise<RecordModel> {
+		return this.collection('users').create({
+			email,
+			password,
+			passwordConfirm
+		});
+	}
+
+	logout(): void {
+		this.authStore.clear();
+	}
+
+	get isLoggedIn(): boolean {
+		return this.authStore.isValid;
+	}
+
+	get getCurrentUser(): AuthRecord {
+		return this.authStore.record;
+	}
+}
+
+export const pocketBase: ExtendedPocketBase = new ExtendedPocketBase(PUBLIC_POCKETBASE_API_URL);
